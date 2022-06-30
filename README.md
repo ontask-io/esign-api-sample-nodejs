81c1c0292831252ed9f22dfb5080fb91b5814b63
Jamison Prianos <jprianos@accusoft.com>
2022-06-29 23:41:31 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,33 @@
+[<img src="https://s3.amazonaws.com/usdphosting.accusoft-ontask/wp-content/uploads/Ontask-API-Logo-2.png.webp" width="600"/>](https://www.ontask.io/solutions/ontask-api/)
+
+# OnTask eSignature API Node.js Sample
+
+## Requirements
+
+- [Node.js 16.x](https://www.python.org/) or higher
+
+- You will first need to [sign up for an API developer account](https://app.ontask.io/signup?source=eSigApi) with OnTask and retrieve your API key.
+
+- Within the code of `example-email.js` and `example-link.js` in the `root` folder insert the API key  you received after creating your account  in the `apiKey` variable within each page.
+
+- You will need to install use `npm install` to install the projects dependencies.
+
+## Samples
+
+There are two examples included with the package which can be found in the root project folder:
+
+- `example-email.js` - will send the signature request to the email address set in the `emailSigner` variable and then send the completed document to the email address configured in the `emailFinalized` variable.
+
+Update these to your liking and then use `npm run email` to execute the sample.  
+
+- `example-link.js` will return a link for you to provide the signer of the document, and once completed send the completed document to the email address configured in the `emailFinalized` variable.
+
+To run the sample update the variables above and use `npm run link`.
+
+## About OnTask
+
+Building secure, compliant eSignature capabilities into your application takes up valuable time and development resources. Fortunately, we've already done all that hard work for you. With [OnTask API](https://www.ontask.io/solutions/ontask-api/), you can bridge the gaps between systems by integrating our proven eSignature and forms workflow functionality into your software. OnTask is reliable and secure; and takes the guesswork out of compliance by meeting regulations for HIPAA, SOC 2, and more. 
+
+We've got all the documentation samples you need to get up and running within a matter of minutes so you can spend more time working on the innovative features that will make your application a success.
+
+Choose from a library of templates or create your own workflows using flexible conditional logic. OnTask's intuitive, easy-to-navigate interface allows you to start solving problems right away without weeks of tedious onboarding and complex integrations.
---
+++ example-link.js
@@ -0,0 +1,80 @@
+
+// Get your free API Key at: https://www.ontask.io/solutions/ontask-api/
+// Full OnTask API Documentation: https://docs.ontask.io/#overview
+const fs = require('fs');
+const fetch = require('node-fetch');
+
+const ontaskUrl = 'https://app.ontask.io/api/v2';
+const apiKey = 'INSERT-API-KEY';
+const emailFinalized = "completed@example.com";
+
+// Upload the file and get a documentid to pass to create a signature endpoint
+// https://docs.ontask.io/#upload
+const uploadSampleDocument = async () => {
+
+    const readStream = fs.createReadStream('./sample-files/sample.pdf');
+
+    const response = await fetch(`${ontaskUrl}/documents`, {
+        method: 'POST',
+        headers: {
+            'Authorization': apiKey,
+            'Content-Type': 'application/pdf'
+        },
+        body: readStream
+    });
+
+    const { documentId } = await response.json();
+    return documentId;
+}
+
+// Call /signature endpoint to start signature process
+// https://docs.ontask.io/#signature-api
+const startSignature = async (documentId) => {
+
+    const requestBody = JSON.stringify({
+        documentId: documentId,
+        testMode: true,
+        signers: [
+            {
+                label: 'John Smith',
+                contactMethod: [
+                    {
+                        type: 'link'
+                    }
+                ]
+            }
+        ],
+        "onSignaturesFinalized": [
+            {
+                type: 'email',
+                email: emailFinalized
+            }
+        ]
+    });
+
+    const response = await fetch(`${ontaskUrl}/signatures`, {
+        method: 'POST',
+        headers: {
+            'Authorization': apiKey,
+            'Content-Type': 'application/json'
+        },
+        body: requestBody
+    });
+
+    const signatureResponse = await response.json();
+    return signatureResponse;
+}
+
+(async () => {
+    console.log('\n##### Example Starting #####')
+
+    const documentId = await uploadSampleDocument();
+    console.log(`\nUploaded sample file and received a documentId of ${documentId}`)
+
+    const response = await startSignature(documentId);
+    console.log('\nURL for signer John Smith: (Follow URL to complete signature process)')
+    console.log(`${response['signers'][0]['contactMethod'][0]['taskUrl']}\n`);
+    console.log(`Once signed the final document will be sent to: ${emailFinalized}\n`)
+
+    console.log('##### Example Complete #####\n')
+})();
